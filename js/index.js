9f7dd8b5f6d09c05765978c86c455bcadea2e530
Coe M <madisonmacf@gmail.com>
2019-12-05 18:16:16 -0500

    link to portfolio added

--- js/index.js
+++ js/index.js
@@ -76,6 +76,7 @@ function hexagonInteract() {
 }
 
 // BEE CHART     BEE CHART     BEE CHART     BEE CHART     BEE CHART     BEE CHART
+
 google.charts.load("current", {
   packages: ["bar"]
 });
@@ -106,3 +107,33 @@ function drawChart() {
 
   chart.draw(data, google.charts.Bar.convertOptions(options));
 }
+
+// MODAL    MODAL    MODAL     MODAL    MODAL     MODAL
+
+var modal = document.getElementById("modal");
+var btn = document.getElementById("open");
+var span = document.getElementById("close");
+
+btn.addEventListener(
+  "click",
+  function(event) {
+    modal.style.display = "block";
+  },
+  false
+);
+
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
